b6d37926dcf027af97ef7469e0ec87da2fa5bd5e
Arun652 <70481319+Arun652@users.noreply.github.com>
2021-06-15 15:22:24 +0530

    Create swap_int.py
    Python program to swap two elements in a list
    Difficulty Level : Easy

---
+++ swap_int.py
@@ -0,0 +1,13 @@
+def swap(list):
+    l=len(list)
+    temp=list[a-1]
+    newlist[a-1]=list[b-1]
+    newlist[b-1]=temp
+    return(newlist)
+
+
+list=[int(i) for i in input('variables').split()]
+a=int(input())
+b=int(input())
+newlist=list
+print (swap(newlist))

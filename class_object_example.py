6ea33c7716c1431ca96ced63f36412c7dd379123
Arun652 <70481319+Arun652@users.noreply.github.com>
2021-07-07 01:56:18 +0530

    Update class_object_example.py
    Write a Person class with an instance variable,age , and a constructor that takes an integer,inital Age, as a parameter. The constructor must assign inital Age to  age after confirming the argument passed as inital Age is not negative; if a negative argument is passed as inital Age , the constructor should set age to 0 and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
    
    1. yearPasses() should increase the age  instance variable by1 .
    2.amIOld() should perform the following conditional actions:
         If ,age<13 print You are young..
         If  age>13 and age<=13 , print You are a teenager..
         Otherwise, print You are old..

--- class_object_example.py
+++ class_object_example.py
@@ -4,9 +4,7 @@ class Person:
         if(self.initalAge<0):
             print("Age is not valid, setting age to 0.")
             self.initalAge=0
-        # Add some more code to run some checks on initialAge
     def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
         if(self.initalAge<13):
             print("You are young.")
         elif(self.initalAge>13 or self.initalAge<=20):
@@ -14,7 +12,6 @@ class Person:
         else:
             print("You are old.")
     def yearPasses(self):
-        # Increment the age of the person in here
         self.initalAge=self.initalAge+1
 t = int(input())
 for i in range(0, t):
